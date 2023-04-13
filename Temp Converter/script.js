d2464f2ff892666df13bd042b42a03e69806b66a
Julia <38337770+juliadoom@users.noreply.github.com>
2023-04-13 13:20:45 -0600

    Update script.js

--- Temp Converter/script.js
+++ Temp Converter/script.js
@@ -2,7 +2,7 @@ document.getElementById('convert').onclick = tempConvert;
 document.getElementById('clear').onclick = clearForm;
 
 function tempConvert() {
- let farenheit = document.getElementById('fahrenheit').value;
+ let fahrenheit = document.getElementById('fahrenheit').value;
  let celsius = document.getElementById('celsius').value;
  
  if (fahrenheit != '') {
