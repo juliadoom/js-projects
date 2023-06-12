2d61e2f7078f01a66099f3ac127af50722ffd49e
Julia <38337770+juliadoom@users.noreply.github.com>
2023-06-12 14:52:09 -0600

    Update script.js

--- CSS JS Clock/script.js
+++ CSS JS Clock/script.js
@@ -1,9 +1,12 @@
 const secondHand = document.querySelector('.second-hand');
-function setDate() {
-  const now = new Date();
-  const seconds = now.getSeconds();
-  const secondsDegrees = (seconds / 60) * 360;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  console.log(seconds);
-}
-setInterval(setDate, 1000);
+      const minuteHand = document.querySelector('.min-hand');
+      const hourHand = document.querySelector('.hour-hand');
+      
+      function setDate() {
+        const now = new Date();
+        const seconds = now.getSeconds();
+        const secondsDegrees = ((seconds / 60) * 360) + 90;
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+        console.log(seconds);
+      }
+      setInterval(setDate, 1000);
