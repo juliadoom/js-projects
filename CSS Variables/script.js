4bd9531e7314e63c1430617aa1cc932360353e36
Julia <38337770+juliadoom@users.noreply.github.com>
2023-06-28 16:35:36 -0600

    Update script.js

--- CSS Variables/script.js
+++ CSS Variables/script.js
@@ -1,7 +1,12 @@
 const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
-  console.log(this.value);
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
 }
 
 inputs.forEach(inputs => inputs.addEventListener('change', handleUpdate));
+inputs.forEach(inputs => inputs.addEventListener('mousemove', handleUpdate));
