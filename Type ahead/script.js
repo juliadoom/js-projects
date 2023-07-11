564e74b03bb08e4535eed742b96595328d058750
Julia <38337770+juliadoom@users.noreply.github.com>
2023-07-11 16:29:41 -0600

    Update script.js

--- Type ahead/script.js
+++ Type ahead/script.js
@@ -1,8 +1,14 @@
 const endpoint =
-  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+        'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-const cities = [];
+      const cities = [];
 
-fetch(endpoint)
-  .then(blob => blob.json())
-  .then(data => console.log(data));
+      fetch(endpoint)
+        .then(blob => blob.json())
+        .then(data => cities.push(...data));
+
+      function findMatches(wordToMatch, cities) {
+        return cities.filter(place => {
+          // city or state matches what was searched?
+        });
+      }
