6670027a9bca99540e917f2c2f78d4466f0cbe01
Julia <38337770+juliadoom@users.noreply.github.com>
2023-08-24 16:28:04 -0600

    Update script.js

--- Dev Tools/script.js
+++ Dev Tools/script.js
@@ -31,11 +31,29 @@ console.info('Rats can tread water for 3 days');
 console.assert(1 === 1, 'That is wrong!');
 
 // clearing
+console.clear();
 
-// Viewing DOM Elements
+// // Viewing DOM Elements
+// console.log(p);
+// console.dir(p);
 
 // Grouping together
+dogs.forEach(dog => {
+  console.group(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+  console.groupEnd(`${dog.name}`);
+});
 
 // counting
+console.count('Julia');
 
 // timing
+console.time('fetching data');
+fetch('https://api.github.com/users/juliadoom')
+  .then(data => data.json())
+  .then(data => {
+    console.timeEnd('fetching data');
+    console.log(data);
+  });
